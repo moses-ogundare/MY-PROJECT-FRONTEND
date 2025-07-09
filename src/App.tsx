2c3f970d20553d941c7feee117e1309dd329e6d6
moses-ogundare <mosesogundare45@gmail.com>
2025-07-09 21:02:45 +0100

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black text-white'>
+       Welcome to our frontend project 
+      </div>  
+    </>
+  )
+}
+
+export default App
